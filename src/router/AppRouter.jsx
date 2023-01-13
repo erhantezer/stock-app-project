0de074da79c8697d58fe2c5a2ac26ec6f6c04214
Erhan <erhantezer68@gmail.com>
2023-01-14 00:36:56 +0300

    updated

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from '../pages/Dashboard'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+        <Route path='register' element={<Register/>} />
+        <Route path='stock' element={<Dashboard/>}/>
+        <Route/>
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
