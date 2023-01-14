93ab4da4e349100b7405bd3f77cc899f0b781153
Erhan <erhantezer68@gmail.com>
2023-01-14 13:45:26 +0300

    pages edited

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { store } from "./app/store";
 import AppRouter from "./router/AppRouter";
 
 
 function App() {
   return (
-    <div>
-      <AppRouter/>
-    </div>
+    <>
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+    <ToastContainer/>
+    </>
   )
 }
 
