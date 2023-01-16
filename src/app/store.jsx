73fb2c6970fbf2e2361a7bd562c0ee7b2c5f5b27
Erhan <erhantezer68@gmail.com>
2023-01-17 01:22:00 +0300

    firms updated

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { persistReducer,persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, } from "redux-persist";
 import storage from "redux-persist/lib/storage";//! defaults to localStorage for web
-import authReducer from "../features/authSlice"
+import authReducer from "../features/authSlice";
+import stockReducer from "../features/stockSlice";
 
 
 
@@ -15,6 +16,7 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
     reducer: {
         auth: persistedReducer,
+        stock: stockReducer,
     },
 
     middleware: (getDefaultMiddleware) =>
--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { Box, Button, Grid, Typography } from '@mui/material'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import FirmCard from '../components/FirmCard';
+import { flexCenter } from '../styles/globalStyle';
+
+
+const BASE_URL = "https://13549.fullstack.clarusway.com";
 
 const Firms = () => {
+  const { token } = useSelector((state) => state.auth);
+  const [firms, setFirms] = useState([])
+
+  const getFirms = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/stock/firms/`, {
+        headers: { Authorization: `Token ${token}` },
+      })
+      setFirms(data)
+      console.log(data)
+    } catch (error) {
+
+    }
+  }
+
+  useEffect(() => {
+    getFirms()
+  }, []);
+
   return (
-    <div>Firms</div>
+    <Box>
+      <Typography variant='h4' color="error" mb={5}>
+        Firms
+      </Typography>
+      <Button variant="contained">
+        New Firm
+      </Button>
+
+      {firms?.length > 0 && (
+        <Grid container sx={flexCenter} mt={3}>
+          {firms?.map((firm) => (
+            <Grid item key={firm.id}>
+              <FirmCard firm={firm}  />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
   )
 }
 
