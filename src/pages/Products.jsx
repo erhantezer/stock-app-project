def7f65bceff831168a6b4437a529b972649f16a
Erhan <erhantezer68@gmail.com>
2023-01-18 03:16:08 +0300

    updated

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Box, Button, Typography } from '@mui/material';
+import ProductModal from '../components/modals/ProductModal';
+import ProductsTable from '../components/tables/ProductsTable';
+
 
 const Products = () => {
+
+
   return (
-    <div>Products</div>
+    <Box>
+      <Typography variant="h4" color="error" mb={4}>
+        Products
+      </Typography>
+
+      <Button variant="contained">
+        New Product
+      </Button>
+
+      <ProductModal/>
+
+      <ProductsTable/>
+    </Box>
   )
 }
 
