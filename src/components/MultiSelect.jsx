00ed2023c2944b7168253a97f4311c67340d38f7
Erhan <erhantezer68@gmail.com>
2023-01-20 02:31:00 +0300

    react tremor

--- src/components/MultiSelect.jsx
+++ src/components/MultiSelect.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Box } from '@mui/material';
+import { MultiSelectBox, MultiSelectBoxItem } from '@tremor/react';
+import { flexCenter } from '../styles/globalStyle';
 
 const MultiSelect = () => {
-  return (
-    <div>MultiSelect</div>
-  )
+
+
+    
+    return (
+        <Box sx={flexCenter} mt={3}>
+            <MultiSelectBox
+                defaultValue={undefined}
+                value={undefined}
+                onValueChange={undefined}
+                placeholder="Select..."
+                icon={undefined}
+                maxWidth="max-w-none"
+                marginTop="mt-0"
+            >
+                <MultiSelectBoxItem
+                    text=""
+                    value={undefined}
+                />
+            </MultiSelectBox>
+        </Box>
+    )
 }
 
 export default MultiSelect
