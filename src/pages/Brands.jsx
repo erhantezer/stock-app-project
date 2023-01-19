391510bb74317c5db577cf3fb312b95a5557996d
Erhan <erhantezer68@gmail.com>
2023-01-19 13:37:47 +0300

    updated brands

--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -1,8 +1,13 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const Brands = () => {
   return (
-    <div>Brands</div>
+    <Box>
+        <Typography variant='h4' color="error" mb={2} >
+          Brands
+        </Typography>
+    </Box>
   )
 }
 
